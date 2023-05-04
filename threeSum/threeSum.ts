5b4d8cab93f66237403aa71ba89e89c9888de28e
kyle <kyle@kylelee.dev>
2023-05-04 12:23:02 -0500

    added container with most water

--- threeSum/threeSum.ts
+++ threeSum/threeSum.ts
@@ -1,17 +1,36 @@
 function threeSum(nums: number[]): number[][] {
-  const sumMap = new Map();
-  let left = 0;
-  let right = nums.length - 1;
+  const target = 0;
 
-  while (left < right) {
-    let leftNum = nums[left];
-    let rightNum = nums[right];
+  const res: number[][] = [];
+  const sortedNums = nums.sort((a, b) => {
+    return a - b;
+  });
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i - 1] === nums[i]) continue; //prevent repeats
+    let left = i + 1;
+    let right = nums.length - 1;
+    const currNum = sortedNums[i];
+    while (left < right) {
+      let leftNum = sortedNums[left];
+      let rightNum = sortedNums[right];
 
-    let currSum = leftNum + rightNum;
+      let currSum = leftNum + rightNum + currNum;
 
-    if (currSum < 0) {
+      //increment/decrement one of the pointers based on the sum
+      if (currSum < target) {
+        left++;
+      } else if (currSum > target) {
+        right--;
+      } else {
+        res.push([leftNum, rightNum, currNum]);
+        left++;
+        while (left < right && nums[left] === nums[left - 1]) left++; //prevent repeats
+      }
     }
   }
 
-  return [];
+  return res;
 }
+
+let t: number[] = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(t));
