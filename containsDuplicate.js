918d05a6b52b396b7f086385b96bf4298692df7b
kyle <kyle@kylelee.dev>
2024-10-01 14:20:12 -0400

    update

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,17 +1,16 @@
 function containsDuplicate(nums) {
-    var map = new Map();
-    for (var i = 0; i < nums.length; i++) {
-        var currNum = nums[i];
-        var isDuplicate = map.has(currNum);
-        if (isDuplicate) {
-            return true;
-        }
-        else {
-            map.set(currNum, currNum);
-            continue;
-        }
+  var map = new Map();
+  for (var i = 0; i < nums.length; i++) {
+    var currNum = nums[i];
+    var isDuplicate = map.has(currNum);
+    if (isDuplicate) {
+      return true;
+    } else {
+      map.set(currNum, currNum);
+      continue;
     }
-    return false;
+  }
+  return false;
 }
 var nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
 console.log(containsDuplicate(nums));
